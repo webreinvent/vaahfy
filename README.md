863ce46924fa29c0079a7b01f52b82bfe4ac1b8f
Pradeep <webreinvent@gmail.com>
2021-06-19 15:42:15 +0530

    Files and configuration done

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # vaahfy
 SCSS for Bulma &amp; Buefy
+
+
+```scss
+@import "../../customize/variables";
+```
+
+- https://jenil.github.io/bulmaswatch/ - For themes
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+const fs = require('fs');
+const fs_extra = require('fs-extra');
+
+mix.setPublicPath('./css');
+mix.sass('./build.scss', './css');
+
+
+mix.webpackConfig({
+    watchOptions: {
+
+        aggregateTimeout: 2000,
+        poll: 20,
+        ignored: [
+            '/node_modules/',
+            '/css/',
+
+        ]
+    }
+});
